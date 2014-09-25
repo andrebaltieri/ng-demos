10232bcb3163f6457aad161d9f74e5b6af07ed09
Andre Baltieri <andrebaltieri@hotmail.com>
2014-09-25 19:55:03 -0300

    Versao inicial

---
+++ content/js/startup.js
@@ -0,0 +1,16 @@
+﻿$( document ).ready(function() {
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "positionClass": "toast-top-full-width",
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+});
